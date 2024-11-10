de327fb1d065f41644e3ddd07a6f475e982bb376
Yashwanth A L <yashwanthal2004@gmail.com>
2024-11-10 15:39:53 +0530

    completed the react devtools project

--- 1. React/297-react-devtools/src/components/App.jsx
+++ 1. React/297-react-devtools/src/components/App.jsx
@@ -1,30 +1,21 @@
 import React from "react";
 import Card from "./Card";
+import Avatar from "./Avatar";
 import contacts from "../contacts";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      <Avatar src="https://randomuser.me/api/portraits/men/1.jpg"/>
+      {contacts.map(contact => (
+        <Card
+          name={contact.name}
+          img={contact.imgURL}
+          tel={contact.phone}
+          email={contact.email}
+        />
+      ))}
     </div>
   );
 }
